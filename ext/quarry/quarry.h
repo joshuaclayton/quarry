7d5a98d305caa08bfbb4e5e55cf5ac50c40f1451
Joshua Clayton <jclayton@thoughtbot.com>
2010-08-17 22:18:22 -0400

    Minor cleanup

--- ext/quarry/quarry.h
+++ ext/quarry/quarry.h
@@ -5,8 +5,7 @@ VALUE mgrs_to_lat_long(VALUE klass) {
   VALUE mgrs_grid = rb_iv_get(klass, "@grid");
   char* grid = StringValuePtr(mgrs_grid);
 
-  double lat;
-  double lng;
+  double lat, lng;
 
   Convert_MGRS_To_Geodetic(grid, &lat, &lng);
 
@@ -17,11 +16,11 @@ VALUE mgrs_to_lat_long(VALUE klass) {
 }
 
 VALUE lat_long_to_mgrs(VALUE klass) {
-  VALUE iv_latitude  = rb_funcall(rb_iv_get(klass, "@latitude" ), rb_intern("to_s"), 0);
-  VALUE iv_longitude = rb_funcall(rb_iv_get(klass, "@longitude"), rb_intern("to_s"), 0);
+  VALUE iv_latitude  = rb_funcall(rb_iv_get(klass, "@latitude" ), rb_intern("to_s"), 0),
+        iv_longitude = rb_funcall(rb_iv_get(klass, "@longitude"), rb_intern("to_s"), 0);
 
-  double latitude =  rb_str_to_dbl(iv_latitude,  Qfalse);
-  double longitude = rb_str_to_dbl(iv_longitude, Qfalse);
+  double latitude  = rb_str_to_dbl(iv_latitude,  Qfalse),
+         longitude = rb_str_to_dbl(iv_longitude, Qfalse);
 
   char grid[15];
   Convert_Geodetic_To_MGRS(latitude*DEG_TO_RAD, longitude*DEG_TO_RAD, 5, grid);
