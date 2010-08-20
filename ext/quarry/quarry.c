9fb9307595f694a74233ec2573380d081f074afb
Joshua Clayton <jclayton@thoughtbot.com>
2010-08-20 15:23:28 -0400

    Move things around to reflect proper file structures

--- ext/quarry/quarry.c
+++ ext/quarry/quarry.c
@@ -1,5 +1,4 @@
 # include <ruby.h>
-# include "mgrs.h"
 # include "quarry.h"
 
 void Init_quarry() {
@@ -10,4 +9,27 @@ void Init_quarry() {
 
   VALUE rb_cLatLng = rb_define_class_under(rb_mQuarry, "LatLng", rb_cObject);
   rb_define_method(rb_cLatLng, "to_mgrs", lat_long_to_mgrs, 0);
+}
+
+VALUE mgrs_to_lat_long(VALUE klass) {
+  VALUE mgrs_grid = rb_iv_get(klass, "@grid");
+  char* grid = StringValuePtr(mgrs_grid);
+  double lat, lng;
+
+  Convert_MGRS_To_Geodetic(grid, &lat, &lng);
+
+  VALUE lat_lng_array = rb_ary_new();
+  rb_ary_push(lat_lng_array, rb_float_new(lat*RAD_TO_DEG));
+  rb_ary_push(lat_lng_array, rb_float_new(lng*RAD_TO_DEG));
+  return lat_lng_array;
+}
+
+VALUE lat_long_to_mgrs(VALUE klass) {
+  double latitude  = rb_num2dbl(rb_iv_get(klass, "@latitude")),
+         longitude = rb_num2dbl(rb_iv_get(klass, "@longitude"));
+
+  char grid[15];
+  Convert_Geodetic_To_MGRS(latitude*DEG_TO_RAD, longitude*DEG_TO_RAD, 5, grid);
+
+  return rb_str_new2(grid);
 }
--- ext/quarry/quarry.h
+++ ext/quarry/quarry.h
@@ -1,25 +1,12 @@
+# include "mgrs.h"
+
+#ifndef __quarry_h__
+#define __quarry_h__
+
 # define RAD_TO_DEG 57.29577951308232087 /* 180/PI */
 # define DEG_TO_RAD 0.0174532925199433 /* PI/180 */
 
-VALUE mgrs_to_lat_long(VALUE klass) {
-  VALUE mgrs_grid = rb_iv_get(klass, "@grid");
-  char* grid = StringValuePtr(mgrs_grid);
-  double lat, lng;
+VALUE mgrs_to_lat_long(VALUE);
+VALUE lat_long_to_mgrs(VALUE);
 
-  Convert_MGRS_To_Geodetic(grid, &lat, &lng);
-
-  VALUE lat_lng_array = rb_ary_new();
-  rb_ary_push(lat_lng_array, rb_float_new(lat*RAD_TO_DEG));
-  rb_ary_push(lat_lng_array, rb_float_new(lng*RAD_TO_DEG));
-  return lat_lng_array;
-}
-
-VALUE lat_long_to_mgrs(VALUE klass) {
-  double latitude  = rb_num2dbl(rb_iv_get(klass, "@latitude")),
-         longitude = rb_num2dbl(rb_iv_get(klass, "@longitude"));
-
-  char grid[15];
-  Convert_Geodetic_To_MGRS(latitude*DEG_TO_RAD, longitude*DEG_TO_RAD, 5, grid);
-
-  return rb_str_new2(grid);
-}
+#endif
